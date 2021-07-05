a01b7f1225da6af37cc9c3289b180eac9cafcdc8
Martha Potts <marthajpotts7@gmail.com>
2021-07-05 12:48:42 -0400

    mock up

---
+++ reactive-martha/mock.js
@@ -0,0 +1,24 @@
+let subjectArr = ["MacOs", 
+"File Tree", 
+"File extension", 
+"HTML", 
+"CSS",
+"Advanced CSS", 
+"JavaScript", 
+"Web APIs", 
+"Third Party APIs", 
+"Server Side APIs", 
+"NodeJS", 
+"OOP",
+"Express", 
+"MySQL", 
+"ORM", 
+"MVC", 
+"NoSQL", 
+"PWA", 
+"ReactJS", 
+"MERN", ]; 
+let displaySubjectsEl = document.querySelector('#subjects-display-container');
+
+
+
